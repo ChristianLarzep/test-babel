cc9cdb70e8c5a8ee3aca2c422e02d28aead3b89b
Christian Larzep <christian.dm2@hotmail.com>
2019-01-28 12:56:19 -0600

    test-babel

---
+++ dist/main.js
@@ -0,0 +1,8 @@
+"use strict";
+
+var hello = "hello horrible world";
+var myconstant = 34;
+
+var myfunction = function myfunction() {
+  console.log("This is my function");
+};
---
+++ src/main.js
@@ -0,0 +1,6 @@
+let hello = "hello horrible world";
+const myconstant = 34;
+
+const myfunction = () => {
+  console.log("This is my function");
+}
